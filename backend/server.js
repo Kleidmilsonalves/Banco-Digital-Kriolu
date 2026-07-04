d6f9849941f7eb6078c44bfe5c11d2728ab7d2a3
Kleidmilson <148908445+Kleidmilsonalves@users.noreply.github.com>
2026-07-04 17:13:40 -0100

    Backup antes de integrar frontend no Express

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use(cors());
 app.use(express.json());
